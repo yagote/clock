4a1f52a7fa55ff737e3d544499a6fbe4fac4966f
yagote <yagote@foxmail.com>
2024-03-30 09:20:20 +0800

    调整桌面样式

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
   // 创建浏览器窗口
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 300,
+    height: 300,
+    backgroundColor: '#000000',
     webPreferences: {
       nodeIntegration: true
     }
